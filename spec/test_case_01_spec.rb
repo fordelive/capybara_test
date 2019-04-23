572a776b3e7d0acba9e3ce026e550f105e69e8be
amigo <volodymyr.shk@gmail.com>
2019-04-23 23:07:42 +0300

    tc_02 started

--- spec/test_case_01_spec.rb
+++ spec/test_case_01_spec.rb
@@ -1,24 +1,31 @@
-# Description:
-#     User can open login page via menu
-# STR:
-#   1. Navigate to Home page as User.
-#   2. Go to Menu
-#   3. Click on 'Login' item.
-# ER: User should be redirected to login page
-
 Dir['./classes/*.rb'].sort.reverse!.each {|file| require file}
 
-Capybara.default_selector = :xpath
-Capybara.current_driver = :selenium
-Capybara.app_host = 'https://demoapp.strongqa.com'
+
+USER_LOGIN = ENV.fetch('LOGIN')
+USER_PASSWORD = ENV.fetch('PASSWORD')
 
 RSpec.describe 'Login page can be opened via menu' do
   include RSpec::Matchers
+
   it 'navigates to Login page' do
     page = HomePage.new
     page.load
     page.navigate_login_page
     expect(page).to be_on_login_page
   end
-
 end
+
+RSpec.describe 'User can log in' do
+  include RSpec::Matchers
+
+  context 'when correct credentials are used' do
+    it 'logs in successfilly' do
+      page = LoginPage.new
+      page.load
+      page.log_user_in ENV.fetch('LOGIN'), ENV.fetch('PASSWORD')
+      expect(page).to be_login_successful
+    end
+  end
+end
+
+
